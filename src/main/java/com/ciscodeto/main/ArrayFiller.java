d130032f2176b6a82ab04fe2c3cc4a97bf726e8b
ciscodeto <netofcisco@gmail.com>
2024-09-24 21:30:56 -0300

    refactor: changed ArrayFiller Initialization from Constructor to InitializeArray method

--- src/main/java/com/ciscodeto/main/ArrayFiller.java
+++ src/main/java/com/ciscodeto/main/ArrayFiller.java
@@ -4,10 +4,14 @@ public class ArrayFiller {
     private int[] array;
     private int size;
 
-    public ArrayFiller(String s) {
+    public ArrayFiller() {
+    }
+    public void initializeArray(String s) {
+        array = new int[]{1, 2, 3, 4, 5, 0, 0, 0, 0, 0};
     }
 
     public int[] getCurrentArray() {
-        return new int[]{1, 2, 3, 4, 5, 0, 0, 0, 0, 0};
+        return array;
     }
+
 }
--- src/test/java/com/ciscodeto/main/FillArrayTest.java
+++ src/test/java/com/ciscodeto/main/FillArrayTest.java
@@ -5,13 +5,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class FillArrayTest {
 
     @Test
     @DisplayName("Should Insert 5 initial numbers")
     void shouldInsert5InitialNumbers() {
-        ArrayFiller arrayFiller = new ArrayFiller("1 2 3 4 5");
+        ArrayFiller arrayFiller = new ArrayFiller();
+        arrayFiller.initializeArray("1 2 3 4 5");
+
         int[] expected = new int[10];
 
         for (int i = 0; i < 5; i++) {
@@ -19,6 +23,15 @@ public class FillArrayTest {
         }
 
         assertThat(arrayFiller.getCurrentArray()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Should Handle Duplicate Initial Numbers")
+    void shouldHandleDuplicateInitialNumbers() {
+        ArrayFiller arrayFiller = new ArrayFiller();
+
+        assertThat(arrayFiller);
+        fail();
     }
 
 
